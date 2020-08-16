80033618047ee4ee2fe04b5770dd77b677bef70b
saifulss <saifulss85@gmail.com>
2020-08-16 11:38:27 +0800

    Fix bug not allowing excused routes to properly escape

--- src/middlewares/JwtVerificationMiddleware.ts
+++ src/middlewares/JwtVerificationMiddleware.ts
@@ -3,11 +3,6 @@ import createHttpError from 'http-errors';
 import { JwtService } from '../services/JwtService/JwtService';
 
 export const JwtVerificationMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-  const authHeader = req.header('Authorization');
-  if (!authHeader) return next(new createHttpError.Unauthorized('No authorization header supplied.'));
-  const token = authHeader.replace('Bearer ', '');
-  if (!token) return next(new createHttpError.Unauthorized('No token supplied.'));
-
   const guestRoutes: string[] = [
     '/signup',
     '/login'
@@ -17,15 +12,26 @@ export const JwtVerificationMiddleware = (req: Request, res: Response, next: Nex
     return next();
   }
 
+  const authHeader = req.header('Authorization');
+  if (!authHeader) return next(new createHttpError.Unauthorized('No authorization header supplied.'));
+  const token = authHeader.replace('Bearer ', '');
+  if (!token) return next(new createHttpError.Unauthorized('No token supplied.'));
+
   const jwtService = new JwtService(
     process.env.JWT_RSA_PRIVATE_KEY,
     process.env.JWT_RSA_PUBLIC_KEY
   );
-  const verified = jwtService.verifyToken(token);
+  const tokenPayload = jwtService.verifyToken(token);
 
-  if (!verified) return next(new createHttpError.Unauthorized('Invalid token.'));
+  if (!tokenPayload) return next(new createHttpError.Unauthorized('Invalid token.'));
 
-  console.log('Token verified successfully:', verified);
+  res.locals.user = {
+    id: tokenPayload.id,
+    displayName: tokenPayload.displayName,
+    email: tokenPayload.email
+  };
+
+  console.log('Token verified successfully:', tokenPayload);
 
   return next();
 };
